6abfd3c3e0160d3045dee981c51f67ce68fb5d73
Kasper Arp <ksrp@demant.com>
2026-04-10 09:04:54 +0200

    Update app title, chip color, and set HttpClient timeout
    Changed app bar title to "VM Tips Kuponen" and updated user chip color to primary for better visibility. Set HttpClient timeout to 10 seconds in Program.cs for improved request handling.

--- TipGame.Blazor/Program.cs
+++ TipGame.Blazor/Program.cs
@@ -9,7 +9,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient
 {
-    BaseAddress = new Uri("https://localhost:7048")
+    BaseAddress = new Uri("https://localhost:7048"),
+    Timeout = TimeSpan.FromSeconds(10)
 });
 
 builder.Services.AddMudServices();
